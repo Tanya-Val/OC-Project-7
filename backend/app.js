6616c02e01f9539a27221b12e23cf4c24d8a2b3f
Tanya-Val <valachitv@gmail.com>
2023-05-23 22:31:02 +0200

    Scss init

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const app = express();
 
-
-
 app.use((req, res) => {
-    res.json({ message: 'Your request was successful! finaly!!!' }); 
+    res.json({ message: 'Your request was successful! forth confirm' }); 
  });
 
 module.exports = app;
