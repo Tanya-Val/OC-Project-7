baebe4bd27ac25dcd8ef341e5271bef571ed993c
Tanya-Val <valachitv@gmail.com>
2023-08-23 19:23:05 +0200

    Update Profile.jsx

--- frontend/src/components/profile/Profile.jsx
+++ frontend/src/components/profile/Profile.jsx
@@ -152,94 +152,32 @@ export default function Profile() {
 }
 
 
-
-// import React from 'react';
-// import "./profile.scss";
-// import { useContext, useState, useEffect } from 'react';
+// import React from 'react'
+// import "./profile.scss"
+// import { useContext, useState } from 'react';
+// import { AuthContext } from "../../context/authContext.jsx"
 // import { useQuery, useQueryClient, useMutation } from '@tanstack/react-query';
 // import { makeRequest } from '../../axios';
 // import { useLocation } from 'react-router-dom';
 
-
 // export default function Profile() {
-//     const { pathname } = useLocation();
-//     const userID = pathname.split("/")[2];
+//     const { currentUser, updateUser  } = useContext(AuthContext);
+//     const userID = useLocation().pathname.split("/")[2];
 
-//     const { isLoading, error, data } = useQuery(['user', userID], () =>
+//     // State variables to store user input for each field
+//     const [firstName, setFirstName] = useState('');
+//     const [lastName, setLastName] = useState('');
+//     const [department, setDepartment] = useState('');
+//     const [email, setEmail] = useState('');
+//     const [newPassword, setNewPassword] = useState(''); // Add state for newPassword
+
+
+//     // Fetch user data
+//     const { isLoading, error, data } = useQuery(['user'], () =>
 //         makeRequest.get('/users/find/' + userID).then((res) => {
 //             return res.data;
 //         })
 //     );
-//     console.log(data, "___________")
-
-//     const [profilePicture, setProfilePicture] = useState(null);
-//     const [texts, setTexts] = useState({
-//         firstName: "",
-//         lastName: "",
-//         department: ""
-//     });
-
-//     useEffect(() => {
-//         if (data) {
-//             setTexts({
-//                 firstName: data.firstName || "",
-//                 lastName: data.lastName || "",
-//                 department: data.department || ""
-//             });
-//         }
-//     }, [data]);
-
-//     const handleChange = (e) => {
-//         setTexts((prev) => ({ ...prev, [e.target.name]: e.target.value }));
-//     };
-
-//     const handleFileChange = (e) => {
-//         setProfilePicture(e.target.files[0]);
-//     };
-
-//     const queryClient = useQueryClient();
-
-//     const mutation = useMutation(
-//         (user) => {
-//             return makeRequest.put("/users", user).then(() => user);
-//         },
-//         {
-//             onSuccess: (user) => {
-//                 queryClient.invalidateQueries(['user', userID]);
-//                 console.log(user, "++++++++++++++++++")
-//             },
-//         }
-//     );
-
-
-//     const upload = async (file) => {
-//         const formData = new FormData();
-//         formData.append('file', file);
-
-//         const response = await makeRequest.post('/upload', formData, {
-//             headers: {
-//                 'Content-Type': 'multipart/form-data',
-//             },
-//         });
-
-//         return response.data;
-//     };
-
-
-
-//     const handleUpdate = async (e) => {
-//         e.preventDefault();
-//         let imgUrl = data?.profilePicture; // Use optional chaining to handle undefined data
-//         if (profilePicture) {
-//             const uploadResponse = await upload(profilePicture);
-//             imgUrl = uploadResponse.fileUrl;
-//         }
-
-//         const updatedUser = { ...texts, profilePicture: imgUrl };
-//         console.log(updatedUser); // Log the updated user object
-
-//         mutation.mutate(updatedUser);
-//     };
 
 //     if (isLoading) {
 //         return <div>Loading...</div>;
@@ -249,50 +187,88 @@ export default function Profile() {
 //         return <div>Error: {error.message}</div>;
 //     }
 
+//     // Function to handle form submission
+//     const handleUpdate = async () => {
+//         // Validate input fields
+//         if (!firstName || !lastName || !email) {
+//             console.error('Please fill in all required fields.');
+//             return;
+//         }
+
+//         // Prepare the data to send to the server
+//         const updatedData = {
+//             firstName,
+//             lastName,
+//             department,
+//             email,
+//             password: newPassword, // Add the new password
+//         };
+
+//         // Send the updated data to the server
+//         try {
+//             const response = await makeRequest.put('/users/update/' + userID, updatedData);
+//             console.log('Data updated successfully:', response.data);
+
+
+//             // Update the current user in the context
+//             updateUser(updatedData);
+//         } catch (error) {
+//             console.error('Error updating data:', error);
+//         }
+//     };
+
+
 //     return (
 //         <div className="profile">
 //             <img
-//                 src={data?.profilePicture}
+//                 src={data.profilePicture}
 //                 alt=""
 //                 className="profilePicture"
 //             />
 
-//             <input
-//                 type="file"
-//                 name="profilePicture"
-//                 onChange={handleFileChange}
-//             />
-//             <span>{data?.firstName} {data?.lastName}</span>
+//             <span>{data.firstName} {data.lastName}</span>
 
 //             <div className="form-container">
 //                 <p>Change your personal data</p>
 //                 <form>
 //                     <input
 //                         type="text"
-//                         placeholder={data?.firstName}
-//                         name="firstName"
-//                         value={texts.firstName}
-//                         onChange={handleChange}
+//                         placeholder={data.firstName}
+//                         value={firstName}
+//                         onChange={(e) => setFirstName(e.target.value)}
 //                     />
 
 //                     <input
 //                         type="text"
-//                         placeholder={data?.lastName}
-//                         name="lastName"
-//                         value={texts.lastName}
-//                         onChange={handleChange}
+//                         placeholder={data.lastName}
+//                         value={lastName}
+//                         onChange={(e) => setLastName(e.target.value)}
 //                     />
 
 //                     <input
 //                         type="text"
-//                         placeholder={data?.department}
-//                         name="department"
-//                         value={texts.department}
-//                         onChange={handleChange}
+//                         placeholder={data.department}
+//                         value={department}
+//                         onChange={(e) => setDepartment(e.target.value)}
 //                     />
 
-//                     <button onClick={handleUpdate}>Update</button>
+//                     <input
+//                         type="email"
+//                         placeholder={data.email}
+//                         value={email}
+//                         onChange={(e) => setEmail(e.target.value)}
+//                     />
+
+//                     <input
+//                         type="password"
+//                         placeholder="New password"
+//                         value={newPassword}
+//                         onChange={(e) => setNewPassword(e.target.value)}
+//                     />
+
 //                 </form>
+
+//                 <button onClick={handleUpdate}>Update</button>
 //             </div>
 //         </div>
 //     );
